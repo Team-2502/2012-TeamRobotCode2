9bf76a024d0b69f12d3986854d459e0b62d1958b
U-Toffler\Keehun <Keehun@Toffler.toffler>
2009-12-14 09:20:27 -0600

    PPC603gnu folder

---
+++ PPC603gnu/SimpleTemplate/Debug/ctdt.c
@@ -0,0 +1,80 @@
+/* ctors and dtors arrays -- to be used by runtime system */
+/*   to call static constructors and destructors          */
+/*                                                        */
+/* NOTE: Use a C compiler to compile this file. If you    */
+/*       are using GNU C++, be sure to use compile this   */
+/*       file using a GNU compiler with the               */
+/*       -fdollars-in-identifiers flag.                   */
+
+
+void _GLOBAL__I__Z20FRC_userClassFactoryv();
+
+void _GLOBAL__I_g_stopImageToPCServer();
+
+void _GLOBAL__I__ZN9ErrorBase16globalErrorMutexE();
+
+void _GLOBAL__I__ZN10Ultrasonic6m_taskE();
+
+void _GLOBAL__I_AxisCamera_debugFlag();
+
+void _GLOBAL__I__Z12SetDebugFlag20DebugOutputType_enum();
+
+void _GLOBAL__I_VisionAPI_debugFlag();
+
+void _GLOBAL__I_TrackAPI_debugFlag();
+
+void _GLOBAL__I__ZN9RobotBase10m_instanceE();
+
+void _GLOBAL__I__Z14SetJaguarSpeedjjf();
+
+extern void (*_ctors[])();
+void (*_ctors[])() =
+    {
+    _GLOBAL__I__Z20FRC_userClassFactoryv,
+    _GLOBAL__I_g_stopImageToPCServer,
+    _GLOBAL__I__ZN9ErrorBase16globalErrorMutexE,
+    _GLOBAL__I__ZN10Ultrasonic6m_taskE,
+    _GLOBAL__I_AxisCamera_debugFlag,
+    _GLOBAL__I__Z12SetDebugFlag20DebugOutputType_enum,
+    _GLOBAL__I_VisionAPI_debugFlag,
+    _GLOBAL__I_TrackAPI_debugFlag,
+    _GLOBAL__I__ZN9RobotBase10m_instanceE,
+    _GLOBAL__I__Z14SetJaguarSpeedjjf,
+    0
+    };
+
+void _GLOBAL__D__Z20FRC_userClassFactoryv();
+
+void _GLOBAL__D_g_stopImageToPCServer();
+
+void _GLOBAL__D__ZN9ErrorBase16globalErrorMutexE();
+
+void _GLOBAL__D__ZN10Ultrasonic6m_taskE();
+
+void _GLOBAL__D_AxisCamera_debugFlag();
+
+void _GLOBAL__D__Z12SetDebugFlag20DebugOutputType_enum();
+
+void _GLOBAL__D_VisionAPI_debugFlag();
+
+void _GLOBAL__D_TrackAPI_debugFlag();
+
+void _GLOBAL__D__ZN9RobotBase10m_instanceE();
+
+void _GLOBAL__D__Z14SetJaguarSpeedjjf();
+
+extern void (*_dtors[])();
+void (*_dtors[])() =
+    {
+    _GLOBAL__D__Z20FRC_userClassFactoryv,
+    _GLOBAL__D_g_stopImageToPCServer,
+    _GLOBAL__D__ZN9ErrorBase16globalErrorMutexE,
+    _GLOBAL__D__ZN10Ultrasonic6m_taskE,
+    _GLOBAL__D_AxisCamera_debugFlag,
+    _GLOBAL__D__Z12SetDebugFlag20DebugOutputType_enum,
+    _GLOBAL__D_VisionAPI_debugFlag,
+    _GLOBAL__D_TrackAPI_debugFlag,
+    _GLOBAL__D__ZN9RobotBase10m_instanceE,
+    _GLOBAL__D__Z14SetJaguarSpeedjjf,
+    0
+    };
