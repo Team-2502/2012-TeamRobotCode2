6e24b883d734777b5e5d2681bc78880e3fefefd2
U-Toffler\Keehun <Keehun@Toffler.toffler>
2009-12-26 12:26:44 -0600

    Removed the "Sample Project" header

--- MainCode.cpp
+++ MainCode.cpp
@@ -1,11 +1,5 @@
 #include "WPILib.h"
 
-/**
- * This is a demo program showing the use of the RobotBase class.
- * The SimpleRobot class is the base of a robot application that will automatically call your
- * Autonomous and OperatorControl methods at the right time as controlled by the switches on
- * the driver station or the field controls.
- */ 
 class RobotDemo : public SimpleRobot
 {
 
