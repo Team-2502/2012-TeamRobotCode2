f682e0481caae4b135a70a554f48a84367937d5e
Alex Reinking <nthread@gmail.com>
2011-02-05 16:55:13 -0600

    Fixing branching.

--- MyRobot.cpp
+++ MyRobot.cpp
@@ -6,10 +6,7 @@
 #include "DisplayWrapper.h"
 #include "DriverWrapper.h"
 #include "JoystickWrapper.h"
-<<<<<<< HEAD
 #include "VisionRoutines.h"
-=======
->>>>>>> c0cc8be37dc7ea7c4bbbdc524ac8a1082b3629c4
 
 class BetaRobot : public IterativeRobot
 {
@@ -18,18 +15,8 @@ class BetaRobot : public IterativeRobot
 		{
 			joystick = new JoystickWrapper(1, Extreme3DPro);
 			joystick->SetSnapPoints(4);
-<<<<<<< HEAD
 			display = new DisplayWrapper;
 			vis = Vision::GetInstance();
-		}
-		
-		void RobotInit(void) {/*pSystem->start();*/}
-		void DisabledInit(void) {/*pSystem->stop();*/}
-		void AutonomousInit(void) {/*pSystem->setCompressor(true);*/}
-		void TeleopInit(void) {/*pSystem->start();*/}
-		
-		void AutonomousPeriodic(void) {display->PrintfLine(8, "We're in autonomous mode!");}
-=======
 			driver = new DriverWrapper(Mecanum, 7, 2, 3, 1);
 			driver->GetRobotDrive()->SetSafetyEnabled(false);
 			display = new DisplayWrapper();
@@ -39,12 +26,8 @@ class BetaRobot : public IterativeRobot
 		void DisabledInit(void) {}
 		void AutonomousInit(void) {}
 		void TeleopInit(void) {}
+		void AutonomousPeriodic(void) {}
 		
-		void AutonomousPeriodic(void) 
-		{
-		}
-		
->>>>>>> c0cc8be37dc7ea7c4bbbdc524ac8a1082b3629c4
 		void TeleopPeriodic(void)
 		{
 			float x, y;
@@ -94,10 +77,7 @@ class BetaRobot : public IterativeRobot
 			float t = -1.0 * joystick->GetThrottle();
 			display->SetScrollLocation(t);
 			display->Output();
-<<<<<<< HEAD
-=======
 			driver->Drive(x, y, joystick->GetRotation());
->>>>>>> c0cc8be37dc7ea7c4bbbdc524ac8a1082b3629c4
 		}
 		
 		/** Unused functions */
@@ -105,18 +85,12 @@ class BetaRobot : public IterativeRobot
 		void DisabledContinuous(void) {}
 		void AutonomousContinuous(void) {}
 		void TeleopContinuous(void) {}
-<<<<<<< HEAD
+		
 	private:
 		DisplayWrapper* display;
 		JoystickWrapper* joystick;
 		Vision* vis;
-=======
-		
-	private:
-		DisplayWrapper* display;
 		DriverWrapper* driver;
-		JoystickWrapper* joystick;
->>>>>>> c0cc8be37dc7ea7c4bbbdc524ac8a1082b3629c4
 };
 
 START_ROBOT_CLASS(BetaRobot);
