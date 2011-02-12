f2a34acf311935260bb43191ffda3ed350a8d6e4
Aaron Schmitz <krajschmitz@gmail.com>
2011-02-12 11:25:50 -0600

    Added camera resolution and put in some tentative motor channels for the arm.

--- config.h
+++ config.h
@@ -6,9 +6,9 @@
 #define REAR_LEFT_CHANNEL 1
 #define FRONT_RIGHT_CHANNEL 4
 #define REAR_RIGHT_CHANNEL 2
-#define WINCH_CHANNEL 314159 //dummy
-#define LEFT_CLAW_CHANNEL 314159 //dummy
-#define RIGHT_CLAW_CHANNEL 314159 //dummy
+#define WINCH_CHANNEL 5 //tentative
+#define LEFT_CLAW_CHANNEL 6 //tentative
+#define RIGHT_CLAW_CHANNEL 314159 //tentative
 #define TWO_CHANNEL_ACCEL_SLOT 1 //tentative
 #define TWO_CHANNEL_ACCEL_X 11   //tentative
 #define TWO_CHANNEL_ACCEL_Y 12   //tentative
@@ -33,5 +33,7 @@
 #define PID_I 0 //7
 #define PID_D 0 //20
 
+#define XRESOLUTION 314 //IDK what this should be
+#define YRESOLUTION 314 //IDK what this should be
 
 #endif
