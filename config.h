67282fda440e09469600750d24d70e68d5315294
Aaron Schmitz <krajschmitz@gmail.com>
2011-02-19 11:11:20 -0600

    Added some defines

--- config.h
+++ config.h
@@ -6,7 +6,7 @@
 #define REAR_LEFT_CHANNEL 1
 #define FRONT_RIGHT_CHANNEL 4
 #define REAR_RIGHT_CHANNEL 2
-#define WINCH_CHANNEL 5 //tentative
+#define WINCH_CHANNEL 10 //tentative
 #define LEFT_CLAW_CHANNEL 6 //tentative
 #define RIGHT_CLAW_CHANNEL 314159 //tentative
 #define TWO_CHANNEL_ACCEL_SLOT 1 //tentative
@@ -18,8 +18,9 @@
 #define LEFT_CLAW_ENCODER_B_CHANNEL 314159 //dummy
 #define ARM_CHAIN_ENCODER_A_CHANNEL 314159 //dummy
 #define ARM_CHAIN_ENCODER_B_CHANNEL 314159 //dummy
-#define SERVO_CHANNEL 7
-#define DIGITAL_SIDECAR_PORT 1
+#define SERVO_CHANNEL_START 7
+
+#define DIGITAL_SIDECAR_PORT 4
 
 #define USE_GYRO
 #ifdef USE_GYRO
@@ -35,8 +36,8 @@
 #define PID_I 0 //7
 #define PID_D 0 //20
 
-#define XRESOLUTION 640 //IDK what this should be
-#define YRESOLUTION 480 //IDK what this should be
+#define XRESOLUTION 320 //IDK what this should be
+#define YRESOLUTION 240 //IDK what this should be
 
 #define ROT_EXPONENTIAL 50 // Tweak this
 #define MAG_EXPONENTIAL 10
